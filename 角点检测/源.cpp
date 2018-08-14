39227a03c54bd5bbae0497d48450cc8be940cb0f
LINGZing <42318665+LINGZing@users.noreply.github.com>
2018-08-14 15:19:18 +0800

    去掉中文

--- 角点检测/源.cpp
+++ 角点检测/源.cpp
@@ -8,18 +8,15 @@ int main()
 	Mat srcImage = imread("1.jpg", 0);
 	Mat dstImage, binImage;
 
-	imshow("原始图", srcImage);
+	imshow("SRCIMAGE", srcImage);
 
-	cornerHarris(srcImage, dstImage, 2, 3, 0.016);
-	imshow("效果图", dstImage);
+	cornerHarris(srcImage, dstImage, 2, 3, 6);
+	imshow("CORNERHARRIS", dstImage);
 
-	threshold(dstImage, binImage, 0.00001, 255, THRESH_BINARY);
-	imshow("二值化", binImage);
-	
+	threshold(dstImage, binImage, -0.07, 255, THRESH_BINARY);
+	imshow("DSTIMAGE", binImage);
+
 	waitKey(0);
 
 	return 0;
 }
-//角点检测
-
-//创建第一个分支
